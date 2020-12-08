6bc670fc94da726a3cb6a7c411b31b78cd62c97a
Marianne Grimstad <marianne.grimstad@gmail.com>
2020-12-08 16:16:52 +0100

    last commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import RecipeList from "./components/recipes/RecipeList";
+import './App.css';
+
+function App() {
+  return (
+    
+    <RecipeList /> 
+    
+  );
+}
+
+export default App;
